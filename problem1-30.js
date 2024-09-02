39a4cc93f1f5b5b1fdab6f4cfa837df333352d5e
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-02 19:42:25 +0600

    problem solve -4

--- problem1-30.js
+++ problem1-30.js
@@ -21,3 +21,15 @@ const roll = rollNumbers.sort(function(a,b){
   return a-b;
 })
 console.log(roll);
+
+//kno year leep year kina ber kro
+
+const isLeapYear = (year) =>{
+  if (year % 400 === 0 || (( year % 4 === 0) && ( year % 100 !== 0))){
+    console.log(`${year} is a leap year`)
+  }else{
+        console.log(`${year} is not a leap year`);
+  }
+}
+
+isLeapYear(2020);
