3c47d873d9a7c636ddccc7859867ea3f1deef661
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-02 11:47:35 +0600

    solveing problem

--- problem1-30.js
+++ problem1-30.js
@@ -1,4 +1,10 @@
 //ludu te kivabe randomly number print krte pari 1-6
 
-const sixsa = Math.floor(Math.random() *6 + 1)
+const sixsa = Math.floor(Math.random() *5 + 1) + 1;
 console.log(sixsa)
+
+function randomNumber (min ,max){
+  return Math.floor(Math.random() * (max - min) + 1 ) + min ;
+}
+
+console.log(randomNumber (1,6));
